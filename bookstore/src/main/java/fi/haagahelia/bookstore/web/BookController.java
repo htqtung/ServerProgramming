65ef5b57aa1cf91948a3d5c924d75bc7524ef2ff
htqtung <31605439+htqtung@users.noreply.github.com>
2018-02-25 23:10:54 +0200

    BookStore
    Bookstore step 1

---
+++ bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -0,0 +1,14 @@
+package fi.haagahelia.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+	
+	@RequestMapping("/index")
+	public String helloIndex() {
+		return "helloIndex";
+	}
+
+}
