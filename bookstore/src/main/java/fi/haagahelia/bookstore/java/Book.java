ffe07f49be50d1007bbb1fe63c9caa6c0edeb3b2
htqtung <31605439+htqtung@users.noreply.github.com>
2018-03-01 19:57:40 +0200

    Updated to chapter 4
    Working bookstore page with add, edit, delete function using JPA and H2.
    Added Category entity

--- bookstore/src/main/java/fi/haagahelia/bookstore/java/Book.java
+++ bookstore/src/main/java/fi/haagahelia/bookstore/java/Book.java
@@ -1,22 +1,59 @@
 package fi.haagahelia.bookstore.java;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
 	// title, author, year, isbn, price
 	private String title;
 	private String author;
 	private int year;
 	private String isbn;
-	private float price;
-
-	public Book(String title, String author, int year, String isbn, float price) {
+	private double price;
+	
+	//One-To-Many Relationship
+	@ManyToOne
+	@JoinColumn(name = "categoryid")
+	private Category category;
+	
+	public Book() {}
+	
+	public Book(String title, String author, int year, String isbn, double price, Category category) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category;
+	}
+	
+	
+	public Category getCategory() {
+		return category;
 	}
 
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
@@ -41,19 +78,19 @@ public class Book {
 		this.year = year;
 	}
 
-	public String getISBN() {
+	public String getIsbn() {
 		return isbn;
 	}
 
-	public void setISBN(String isbn) {
+	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
 
-	public float getPrice() {
+	public double getPrice() {
 		return price;
 	}
 
-	public void setPrice(float price) {
+	public void setPrice(double price) {
 		this.price = price;
 	}
 
--- bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,14 +1,59 @@
 package fi.haagahelia.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.haagahelia.bookstore.java.Book;
+import fi.haagahelia.bookstore.java.BookRepository;
+import fi.haagahelia.bookstore.java.CategoryRepository;
 
 @Controller
 public class BookController {
+	
+	@Autowired
+	private BookRepository repository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@RequestMapping("/index")
 	public String helloIndex() {
 		return "helloIndex";
 	}
-
+	
+	@RequestMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "bookList";
+	}
+	
+	@RequestMapping("/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		model.addAttribute("categories", categoryRepository.findAll());
+		return "createPage";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveBook(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.delete(bookId);
+		return "redirect:../booklist";
+	}
+	
+	@RequestMapping("/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findOne(bookId));
+		model.addAttribute("categories", categoryRepository.findAll());
+		return "editPage";
+	}
+	
 }
